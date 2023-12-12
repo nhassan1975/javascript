7d8e0b53b1dea69fbe3054e30b06b292e07deb64
nhassan1975 <146669656+nhassan1975@users.noreply.github.com>
2023-12-12 12:40:28 +0200

    first commit

---
+++ js/task.js
@@ -0,0 +1,40 @@
+/**************************************task1/21*************************************/
+//1- A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
+
+//- The wanted word is in lowercase.
+//- The crowd of letters is all in uppercase.
+//- Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
+//Hint ==> the input string will be something like this "SDNIDWDWDNDwordKJNJSDN".
+/*------------------------------------------------------------------------*/
+var a=["SDNIDWDWDNDwordKJNJSDN"];
+function detect(lowercase) {
+    for (let i = 0; i < a.length; i++)
+    return String.lowercase
+};
+console.log(a.lowercase);
+/*0000000000000000000000000000000000000000000000000000000000000000000000000000*/
+var a=["SDNIDWDWDNDwordKJNJSDN"];
+function split(lowercase) {
+    return lowercase
+}
+console.log(lowercase )
+var s=[];
+s.split(lowercase);
+console.log(lowercase);
+var str=["SDNIDWDWDNDwordKJNJSDN"]
+function split_small(str) {
+    split(a-z)
+}
+result (split_small(str));
+console.log(result);
+/*00000000000000000000000000000000000000000000000000000000000000000000000000000*/
+const word_find=(string)=>{
+    let word="" ;
+    for (let i = 0; i < i.length; i++) {
+        if (string[i]=string[i].toLowerCase()) {
+            word= string[i];
+        };
+    }
+    return word ;
+}
+console.log(word_find("SDNIDWDWDNDwordKJNJSDN"));
